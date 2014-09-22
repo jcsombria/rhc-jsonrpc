ac158aebf757385e8889a8f12e57a57151cbb282
Jesús <jcsombria@gmail.com>
2014-09-22 17:45:16 +0200

    + Code cleaned

--- JsonRpcBuilder.js
+++ JsonRpcBuilder.js
@@ -19,37 +19,8 @@
  */
 
 var JsonRpcBuilder = {
-	/** ID for rpc calls */
-//	id: 0,
-	/** List of method calls to process in batch mode*/
-//	batch: [],
-	/** List of method calls sent to the server */
-//	sent: [],
-	/** List of method responses from the server */
-//	response: [],	
-
-	/** JSON-RPC Templates */
-	template: {
-		METHOD: '${METHOD}',
-		PARAMS: '${PARAMS}',
-		ID: '${ID}',
-		REQUEST: '{jsonrpc: "2.0", method: ${METHOD}, params: ${PARAMS}, id: ${ID}}',
-		RESPONSE_OK: '{jsonrpc: "2.0", result: ${RESULT}, params: ${PARAMS}, id: ${ID}}',
-		RESPONSE_ERROR: '{jsonrpc: "2.0", method: ${METHOD}, params: ${PARAMS}, id: ${ID}}',
-		ERROR: '{code: "2.0", message: ${MESSAGE}, data: ${DATA}}'
-	},
-
-	/**
-	 * Build a JSON-RPC request object
-	 *
-	 * @param {string} method - The method to call
-	 * @param {array|object} params - The params either by position (array) or 
-     *                                by name (object).
-	 * @param {string|integer} id - The id of the call
-	 */
 	request: function(method, params, id) {
 		if(params && !(params instanceof Array)) throw new InvalidParamsException();
-//		var pars = (params && !(params instanceof Array)) ? [params] : params;
 		var request = {jsonrpc: '2.0', method: method};
 		if(params!==undefined) request.params = params;
 		if(id!==undefined) request.id = id;
@@ -57,12 +28,6 @@ var JsonRpcBuilder = {
 		return request;
 	},
 
-	/**
-	 * Build a JSON-RPC response object
-	 *
-	 * @param {object} result - The result to return
-	 * @param {string|integer} id - The id of the call
-	 */
 	response: function(result, id) {
 		return {
 			jsonrpc: '2.0',
@@ -71,12 +36,6 @@ var JsonRpcBuilder = {
 		}
 	},
 
-	/**
-	 * Build a JSON-RPC error response object
-	 *
-	 * @param {object} error - The error object
-	 * @param {string|integer} id - The id of the call
-	 */
 	responseWithError: function(error, id) {
 		return {
 			jsonrpc: '2.0',
@@ -85,13 +44,6 @@ var JsonRpcBuilder = {
 		};
 	},
 
-	/**
-	 * Build a JSON-RPC error object
-	 *
-	 * @param {integer} code - The result to return
-	 * @param {string} message - The error description message 
-	 * @param {object} data - Additional data
-   */
 	error: function(code, message, data) {
 		return {
 			code: code, 
@@ -100,19 +52,10 @@ var JsonRpcBuilder = {
 		};
 	},
 
-	/**
-	 * Send method calls in batch list
-	 *
-	 * @param {string} resString - The response in JSON-RPC format
-   */
 	parseResponse: function(resString) {
 		var response = null;
 		try {
 			response = JSON.parse(resString);
-/*			if(this.sent[response.id]) {
-				var method = this.sent[response.id].method;
-				this.response[method] = response;
-			}*/
 		} catch(error) {
 			console.log(error);
 		}
---
+++ server.js
@@ -0,0 +1,53 @@
+/**
+ * JSON-RPC Server Tester
+ * author: Jesús Chacón <jcsombria@gmail.com>
+ *
+ * Copyright (C) 2013 Jesús Chacón
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var http = require('http');
+var rpcserver = require('./HILRemoteServer');
+//rpcserver.setBoard(myboard);
+
+process.env.PORT = 2055;
+process.env.IP = 'localhost';
+
+// Create the http server
+http.createServer(onRequest).listen(process.env.PORT, function() {
+	console.log('Server listening on ' + process.env.IP + ':'+ process.env.PORT);
+});
+
+// Handle the http requests
+function onRequest(request, response) {
+		var headers = {
+			'Content-Type': 'application/json', 
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+			'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+		}
+	var body = '';
+//  request.setEncoding('utf8');
+  request.on('data', function (chunk) {
+    body += chunk;
+  })
+
+  request.on('end', function () {
+		response.writeHead(200, headers);
+console.log(body);
+		response.write(JSON.stringify(rpcserver.parse(body)));
+		response.end();
+	});
+}
