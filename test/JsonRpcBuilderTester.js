e0085e137e786875c8999db63516be3bcc159548
Jesús <jcsombria@gmail.com>
2015-09-10 12:19:35 +0200

    + rhi-server-0.1

--- test/JsonRpcBuilderTester.js
+++ test/JsonRpcBuilderTester.js
@@ -17,8 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-var JsonRpcBuilder = require('./JsonRpcBuilder');
+var JsonRpcBuilder = require('../jsonrpc/JsonRpcBuilder');
 
 var JsonRpcBuilderTester = {
 	testRequest: function() {
